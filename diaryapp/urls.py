e9bcac001f8ff46ceb448c4c5fe283e5d3444c58
inandexhale <inandexhale@gmail.com>
2022-01-23 21:33:40 +0900

    Templates html & static sync

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login),
     path('main/', views.main),
+    path('diary/', views.dairy),
+    path('lecture/', views.lecture),
+    path('news/', views.news),
+    path('hire/', views.hire),
+    
     path('main/test', views.test),
 
     path('/api/news/readAll', views.read_all_news),
