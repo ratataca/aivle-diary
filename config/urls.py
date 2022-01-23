e649254c5609ace5518563bee32addfe8846fb96
ratataca <gywls18@koreatech.ac.kr>
2022-01-23 17:05:08 +0900

    필요 파일 생성 및 파일 정리

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from diaryapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('diaryapp/', include('diaryapp.urls')),
+
 ]
---
+++ diaryapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import User
+
+# TODO - 2. 모델명칭 등록하기
+admin.site.register(User)
