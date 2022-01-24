2a56b72a7798dd446e044bbb9a4add0e4ef9d399
ratataca <gywls18@koreatech.ac.kr>
2022-01-24 20:13:52 +0900

    24일 branch merge done.

--- diaryapp/serializers.py
+++ diaryapp/serializers.py
@@ -1,17 +1,17 @@
-from rest_framework import serializers
-from .models import Lecture,News,Recruit
+# from rest_framework import serializers
+# from .models import Lecture,News,Recruit
 
-class LectureSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Lecture       
-        fields = ['date','topic','detail','professor_name'] 
+# class LectureSerializer(serializers.ModelSerializer) :
+#     class Meta :
+#         model = Lecture       
+#         fields = ['date','topic','detail','professor_name'] 
 
-class NewsSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = News    
-        fields = ['date','title','content','company','link']
+# class NewsSerializer(serializers.ModelSerializer) :
+#     class Meta :
+#         model = News    
+#         fields = ['date','title','content','company','link']
 
-class RecruitSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Recruit  
-        fields = '__all__'
+# class RecruitSerializer(serializers.ModelSerializer) :
+#     class Meta :
+#         model = Recruit  
+#         fields = '__all__'
