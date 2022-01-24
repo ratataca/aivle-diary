8be7314693248c2c7d5b658265259766e42eb69c
ratataca <gywls18@koreatech.ac.kr>
2022-01-24 20:45:43 +0900

    urls.py 경로지정

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'diaryapp'
 urlpatterns = [
     
 
-    # 1. html 파일줘
+    # 1. HTML 파일 경로 지정
     path('login/', views.login, name='login'),
     path('main/', views.main, name='main'),
     path('diary/', views.dairy, name='diary'),
@@ -17,15 +17,31 @@ urlpatterns = [
     
     path('main/test', views.test, name='test'),
 
-    # 2. 이벤트 -> 디비 줘봐( 조회 )
-    #    이벤트 -> 업데이트(수정), 삭제, 입력(create)
+    # 2. 이벤트 
+
+    # 2. 1. 로그인
+    # path('api/user/login', views.login_user, name='login_user'),
+    
+
+    # 2. 2. 회원가입
+    # path('api/user/signup', views.signup_user, name='signup_user'),
+
+    # 2. 3. 로그아웃
+
+    # 2. 4. 뉴스 
     path('api/news/readAll', views.read_all_news, name='news_read_all'),
+
+    # 2. 5. 채용정보 
     path('api/recruit/readAll', views.read_all_recruit, name='recruit_read_all'),
 
+    # 2. 6. 강의
+    path('api/lecture/readAll', views.read_all_lecture, name='lecture_read_all'),
+
+    # 2. 7. TIL관련
+
     # # 회원가입 등록 요청
-    # path('api/user/signup', views.signup_user, name='signup_user'),
+    
 
     # # 로그인 확인 요청
     
-    #  이벤트 -> 업데이트(수정), 삭제, 입력(create)
 ]
