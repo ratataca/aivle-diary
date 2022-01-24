5ed9232fe7f13fb5b25736d2e4d61a9a0c7750f4
ratataca <gywls18@koreatech.ac.kr>
2022-01-24 09:33:07 +0900

    Merge branch 'main'

--- diaryapp/urls.py
+++ diaryapp/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     # client -> server
     # 1. html 파일줘
     path('main/', views.main),
 
     # ratataca.html로 가는 경로
-=======
     path('login/', views.login),
     path('main/', views.main),
     path('diary/', views.dairy),
@@ -16,7 +14,6 @@ urlpatterns = [
     path('news/', views.news),
     path('hire/', views.hire),
     
->>>>>>> e9bcac001f8ff46ceb448c4c5fe283e5d3444c58
     path('main/test', views.test),
 
     # 2. 이벤트 -> 디비 줘봐( 조회 )
